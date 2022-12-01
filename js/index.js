5438bbf4cf7c893fb26e6a1d7034b6113b5f4b72
Brayan Ceron <brayan0np@gmail.com>
2022-12-01 17:11:11 -0500

    br3

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ function add(){
    document.getElementById("divmostrar").innerHTML= cad
 
 }
+
+
+function eliminar(){
+    let nombre = document.getElementById("txt_nombres").value
+    bd=bd.filter((i)=>i!==nombre)
+
+
+    let cad="";
+   bd.forEach(objActual =>cad=cad+objActual+" <br> ");  
+
+   document.getElementById("divmostrar").innerHTML= cad
+}
